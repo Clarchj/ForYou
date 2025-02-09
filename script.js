05c99fac94885074e740ad5c6aff23eadc0b814e
Clarchj <117249851+Clarchj@users.noreply.github.com>
2025-02-09 23:54:27 +0700

    "it's over"

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Mong mình có nhiều Valentine nữa. Anh yêu em nhiều lắm. Anh đón chị bé vào 7h 14/02 nhé :)";
-    gif.src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTNrN2ZmenZ2NXB3YjZyaTI5Z3Q2bjRkcmYwOTlmZ3c1eXcyNnJ2ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9ZsHoKnjg4ykx3mzTR/giphy.gif";
+    question.innerHTML = "Nhưng vẫn chúc em hạnh phúc";
+    // gif.src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTNrN2ZmenZ2NXB3YjZyaTI5Z3Q2bjRkcmYwOTlmZ3c1eXcyNnJ2ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9ZsHoKnjg4ykx3mzTR/giphy.gif";
 
     // Hide the No button
     noBtn.style.display = "none";
